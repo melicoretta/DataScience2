7c6a840377cb9a4d44c9976aea79a3972a5169d5
Coretta Meli Sonhana <coretta.sonhana@stud.h-da.de>
2025-12-26 17:32:18 +0100

    add webpage for mortality prediction

--- data_science/data_science/settings.py
+++ data_science/data_science/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware", # add 26.12.2025
+    "whitenoise.middleware.WhiteNoiseMiddleware", # add 26.12.2025
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,13 +90,15 @@ WSGI_APPLICATION = 'data_science.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = { "default": dj_database_url.config(default="sqlite:///db.sqlite3") }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
